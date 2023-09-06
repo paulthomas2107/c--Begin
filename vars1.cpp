aa3260650ce7f1eda932bf5c7964347f487e8874
Paul Thomas <paulthomas2107@gmail.com>
2023-09-06 18:07:48 +0100

    More

---
+++ vars1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "John";
+    int characterAge = 79;
+
+    cout << "There was once a man called " << characterName << endl;
+    cout << "He was " << characterAge << " old." << endl;
+
+    return 0;
+}
